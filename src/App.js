62c8053cfcbfe1d591a1cde22a2aa83e52417bd9
Anastasiya-Polikanova <123961941+Anastasiya-Polikanova@users.noreply.github.com>
2023-03-27 11:37:16 +0300

    new file:   Book-Search 	deleted:    README.md 	modified:   package-lock.json 	modified:   package.json 	deleted:    public/favicon.ico 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	deleted:    src/logo.svg 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import {AiOutlineSearch} from "react-icons/ai";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [searchBook, setSearchBook] = useState('')
+const [books, setBooks] = useState(null)
+
+ 
+function searchGo(event){
+const Api ='https://www.googleapis.com/books/v1/volumes?q='+  event
+
+axios.get(Api).then(function(response){
+  setBooks(response.data);
+  }).catch(function(error){
+  console.log(error);
+  });
 }
+
+console.log(books);
+
+return (
+  
+  <div className='App'>
+    <h1 className='header'>Book Search</h1>
+
+      <div className='search'>
+          <input type='text' placeholder="Введите поисковой запрос"
+          onChange={e => setSearchBook(e.target.value)} value={searchBook}></input>
+          <AiOutlineSearch className="serch-icon" 
+          onClick={() => searchGo(searchBook)}></AiOutlineSearch>
+      </div>
+
+      <div className='content'>
+          {books && books?.totalItems > 0 ? 
+          books.items.map((elem) => (<div key={elem.id}>
+
+          <div className='info'>
+            <p className='author'>автор: {elem.volumeInfo?.authors && Array.isArray(elem.volumeInfo?.authors) ? 
+              elem.volumeInfo?.authors.join(', '): 'Author not found'}</p>
+            <p className='title'> "{elem.volumeInfo.title}" </p>
+            <p>{elem.volumeInfo.publisher}</p>
+            <p>{elem.volumeInfo.publishedDate}</p>
+
+            <div className='image'>
+              <img src={elem.volumeInfo.imageLinks?.thumbnail ? 
+                elem.volumeInfo.imageLinks.thumbnail : 'https://static.thenounproject.com/png/1527904-200.png'} alt="img"/>
+            </div>
+    
+          </div>
+        </div>)) : <p className='notFound'>Books not found</p> }
+      </div>
+    </div>
+  )
+}
+
 
 export default App;
